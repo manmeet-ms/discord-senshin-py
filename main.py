fb914352c6e9e7ac875c0cb6f152f6b31e499b22
Manmeet Singh <manmeets.zsh@gmail.com>
2023-09-04 23:55:06 +0530

    Footer update

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 quotes = ["A pinch of hardwork everyday, can put you years ahead of your life", "Get out of the matrix","So, you were bornt to feel nice?","Stop! You are geting comfortable!", "Unexpected events occur frequently. Be Alert !Expect the unexpected !!","You will gap in your comfort zone!", "🤞 Trying then fail is better than failing to try", "🐸 Eat the Frog Conquer the struggling tasks like a pro with the Eat the Frog strategy.", "⏲️ 20 second rule; Ikk vaari shuru kar k ta'an dekh technique (for 20 sec)", "⏲️ 3, 2, 1 rule Kill procrastination by moving into action within 3 seconds.", "🚀 Move before you crave for any motivation", "0% Motivation 100% Discipline", "Sugoi! Push past the 40% potential governors barrier.", "Procrastination steals your Time; seize it back with dominance. Procrastination is the thief of Time, your enemy. (Dominate it, before it takes charge on yourself)", "☘️ Dream ↑  Efforts ↑ ( D ∝ E) As your dreams rise, let your efforts soar.", "You must ACT!  Indecision is the enemy; decisive action is the key.", "⚡ Put ENERGY, Get RESULTS Invest your energy, harvest incredible results.", "✂️ Cut fkin' distractions Achievement demands focus; eliminate distractions ruthlessly.", "The harder the battle, the sweeter the victory.", "FEAR is temporary, REGRET is permanent", "🎇 I submit to a higher power; they are acting through me.", "✨ Your worship directs the manifestation process.", "💖 As higher power says, as one worships me, that is how I manifest to them."]
 
 # Discord starts 
-
 intent=discord.Intents.default()
 client=discord.Client(intents=intent)
 
@@ -38,8 +37,9 @@ async def send_message():
     channel_id=1148275606635696208 #pc-config
     channel = client.get_channel(channel_id)
     choice = randint(0, len(embed_colors)-1)
-    embedtry=discord.Embed(title="Finished 45m Pomodoro", description=f"{quotes[choice]} {mention}", color=embed_colors[choice])
-    embedtry.set_footer(text=f"Embed Hex: {hex(embed_colors[choice])}", icon_url=None)
+    embedtry=discord.Embed(description=f"{quotes[choice]} {mention}", color=embed_colors[choice])
+
+    embedtry.set_footer(text=f"Pomodoro Duration: 45m\nEmbed Hex: {hex(embed_colors[choice])}", icon_url=None)
     await channel.send(embed=embedtry)
 
 client.run(DISCORD_TOKEN)
