8eff528c1b4627e63cd069781b033bf383424c5e
Manmeet Singh <95163474+manmeet-ms@users.noreply.github.com>
2023-09-10 07:25:03 +0530

    set one color for the embed

--- main.py
+++ main.py
@@ -56,11 +56,11 @@ async def on_ready():
 async def send_message():
     global quotes
     mention='<@788713878963748876>'
-    embed_colors=[0x64748b, 0xfed7aa, 0xf97316, 0xfef3c7, 0xfbbf24, 0x22c55e, 0x0d9488, 0x22d3ee, 0x0ea5e9, 0x8b5cf6, 0xf9a8d4, 0xf472b6, 0xfda4af]
+    #embed_colors=[0x64748b, 0xfed7aa, 0xf97316, 0xfef3c7, 0xfbbf24, 0x22c55e, 0x0d9488, 0x22d3ee, 0x0ea5e9, 0x8b5cf6, 0xf9a8d4, 0xf472b6, 0xfda4af]
     channel_id=1148275606635696208 #pc-config
     channel = client.get_channel(channel_id)
     choice = randint(0, len(embed_colors)-1)
-    embedtry=discord.Embed(description=f"{quotes[choice]} {mention}", color=embed_colors[choice])
+    embedtry=discord.Embed(description=f"{quotes[choice]} {mention}", color=0xfed7aa)
 
     embedtry.set_footer(text=f"Pomodoro Duration: 45m\nEmbed Hex: {hex(embed_colors[choice])}", icon_url=None)
     await channel.send(embed=embedtry)
